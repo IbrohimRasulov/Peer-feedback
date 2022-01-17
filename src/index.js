3a0529f0c21393959d6ced719b6f8991f6c5f7b2
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-01-18 01:24:47 +0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-import _ from 'lodash';
 import './style.css';
+import { addTask, displayTasksFromLS } from './add&delete';
+
+let tasks = [];
+
+const getFromLS = () => {
+  if (localStorage.length !== 0) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+    displayTasksFromLS(tasks);
+  }
+};
+
+getFromLS();
+
+addTask(tasks);
